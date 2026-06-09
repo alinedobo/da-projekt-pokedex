d5b6f735fd2216f1f3a52ee4c78b0dc225382d33
Aline Dobo <dobo.aline@gmail.com>
2026-06-09 15:31:17 +0200

    submit to Nico for question

--- main.js
+++ main.js
@@ -1,34 +1,49 @@
-console.log("pokepoke");
-
-async function getPokemonNames() {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=0",);
-    const responseFromJson = await response.json();
-
-    console.log(responseFromJson);
-    console.log(responseFromJson.results[0].name);
-
-    renderCards(responseFromJson);
+function init(){
+    getPokemon();
+    renderCards();
 }
 
-getPokemonNames();
+const result = [];
+
+async function getPokemon() {
+    for (let i = 1; i < 10; i++){
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+        let responseFromJson = await response.json();
+
+        result.push(responseFromJson);
+        console.log(result);
+    }
+
+    renderCards(result);
+}
 
 
-function renderCards(arr) {
-
-    console.log(arr.results[0].name + " 2");
-
+function renderCards(result) {
     const cardWrapperRef = document.getElementById("card-wrapper");
 
     cardWrapperRef.innerHTML = "";
-    cardWrapperRef.innerHTML = /*html*/ `
+    
+    for (let j = 0; j < result.length; j++){
+        let pokemonName = result[j].name;
+        let pokemonRank = result[j].id;
+        let pokemonSprite = result[j].sprites.back_default;
+        let pokemonTypeOne = result[j].types[0].type.name;
+        let pokemonTypeTwo = result[j].types[1].type.name;
+        
+        cardWrapperRef.innerHTML += /*html*/ `
         <div class="poke-card">
-            <p>${arr.results[0].name}</p>
-            <img src="${arr.results[0].url}" alt="">
+            <div class="card-head">
+                <p>${pokemonName}</p>
+                <p>${pokemonRank}</p>
+            </div>
+            <div class="card-body">
+                <div class="pokemon-type">
+                    <p>${pokemonTypeOne}</p>
+                    <p>${pokemonTypeTwo}</p>
+                </div>
+                <img src="${pokemonSprite}" alt="">
+            </div>
         </div>
     `;
-}
-
-
-function init(){
-    renderCards();
+    }
 }
