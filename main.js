562f3d3afe59db5de8e0b70dc482cf57917bf806
Aline Dobo <dobo.aline@gmail.com>
2026-06-10 13:30:06 +0200

    we have tabs on the card

--- main.js
+++ main.js
@@ -31,14 +31,12 @@ function renderPokemonSummary(result) {
 
     for (let j = 0; j < result.length; j++) {
         if (result[j].types.length > 1) {
-            let pokemonTypeTwo = result[j].types[1].type.name;
             cardWrapperRef.innerHTML += getTypeTwo(j, result);
         } else {
             cardWrapperRef.innerHTML += getNoTypeTwo(j, result);
         }
     }
 }
-
 
 function showFullCard(j) {
     const dialogRef = document.getElementById("dialog-popup");
@@ -50,4 +48,51 @@ function showFullCard(j) {
     } else {
         pokemonSummaryRef.innerHTML = getNoTypeTwo(j, result);
     }
+
+    const pokemonStatsRef = document.getElementById("pokemon-stats");
+    pokemonStatsRef.innerHTML = /*html*/ `
+    <!-- Tab links -->
+        <div class="tab">
+            <button class="tablinks" onclick="openTab(event, 'About')">About</button>
+            <button class="tablinks" onclick="openTab(event, 'Stats')">Stats</button>
+        </div>
+
+        <!-- Tab content -->
+        <div id="About" class="tabcontent">
+            <h3>About</h3>
+        </div>
+        <div id="Stats" class="tabcontent">
+            <h3>Stats</h3>
+        </div>
+    `;
+
+    const pokemonSliderRef = document.getElementById("pokemon-slider");
+    pokemonSliderRef.innerHTML = /*html*/ `
+        <div>
+            <button>PREVIOUS</button>
+            <button>NEXT</button>
+        </div>
+    `;
 }
+
+
+function openTab(evt, tabName) {
+  // Declare all variables
+  let i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += "active";
+} 
